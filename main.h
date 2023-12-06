d91a31f3371e4be130ca47409e77004a9f2c148f
bjeptum <brendajeptum@gmail.com>
2023-12-06 18:52:07 +0000

    Updated header file

--- main.h
+++ main.h
@@ -2,11 +2,12 @@
 #define MAIN_H
 #include <unistd.h>
 #include <stdarg.h>
-#include <stdio.h>
+
 
 
 int _printf(const char *format, ...);
 char *_itoa(int i, char *strout, int base);
+
 
 int _putchar(char c);
 
