8ecd078d882efb24a05b198db3f1e9d136e2d2f4
bjeptum <brendajeptum@gmail.com>
2023-12-08 16:42:55 +0000

    Update print_all to iterate through the format string

--- print_all.c
+++ print_all.c
@@ -9,15 +9,42 @@ int _printf(const char *format, ...)
 {
 	va_list argList;
 	int count = 0;
+	int i = 0;
 
 	va_start(argList, format);
-
 	/* Function calls for conversion specifiers */
-	print_c_s_prct(argList, &count, format);
-	print_d_i(argList, &count);
-	print_b(argList, &count);
-
+	while (format && format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+		{
+			i++;
+			switch (format[i])
+			{
+			case 'c':
+			case 's':
+			case '%':
+				print_c_s_prct(argList, &count, &format[i]);
+				break;
+			case 'd':
+			case 'i':
+				print_d_i(argList, &count);
+				break;
+			case 'b':
+				print_b(argList, &count);
+				break;
+			default:
+				_putchar('%');
+				_putchar(format[i]);
+				count += 2;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
 	va_end(argList);
-
 	return (count);
 }
