4b3a9bc0464359c56449b0d0f1a4a4df44c02e45
bjeptum <brendajeptum@gmail.com>
2023-12-08 18:24:58 +0000

    Updated format specifier file

---
+++ format_handler.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * handle_format_specifier - iterates through the formats
+ * @format: list of types of arguments passed to the function
+ * @argList: list of arguments
+ * Return: Always 0
+ */
+int handle_format_specifier(va_list argList, char *format)
+{
+	int count = 0;
+	int i = 0;
+
+	switch (format[i])
+	{
+		case 'c':
+		case 's':
+		case '%':
+			print_c_s_prct(argList, &count, &format[i]);
+			break;
+		case 'd':
+		case 'i':
+			print_d_i(argList, &count);
+			break;
+		case 'b':
+			print_b(argList, &count);
+			break;
+		default:
+			_putchar('%');
+			_putchar(format[i]);
+			count += 2;
+	}
+	return (0);
+}
